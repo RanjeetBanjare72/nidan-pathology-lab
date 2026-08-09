903660bc47279b997d7cbb0fcc4a77d927e75e45
RanjeetBanjare72 <141644495+RanjeetBanjare72@users.noreply.github.com>
2026-08-09 18:01:41 +0530

    Fix import statement for supabase

--- app/test-master/page.js
+++ app/test-master/page.js
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
-import import { supabase } from "../../lib/supabase";
+import { supabase } from "../../lib/supabase";
 
 const CATEGORIES = [
   "Hematology",
